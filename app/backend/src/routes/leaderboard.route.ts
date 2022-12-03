ff077c29a2dec27c1413bb672b06e8205ebfd79a
Guilherme Palma <guilhermepallma@gmail.com>
2022-12-03 16:30:01 -0400

    ⚙ correção rota, requisito 29 e 30

--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -4,6 +4,6 @@ import LeaderboardController from '../controllers/leaderboard.controller';
 const leaderboardRoute = Router();
 const leaderboard = new LeaderboardController();
 
-leaderboardRoute.get('/home', leaderboard.getSortFilter);
+leaderboardRoute.get('/home', leaderboard.sortFilterHome);
 
 export default leaderboardRoute;
